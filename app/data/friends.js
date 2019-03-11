4c0a0cb5617ebbefbeae55aa03922be5956b3ec7
amybiltz <amy.biltz@yahoo.com>
2019-03-11 10:57:32 -0700

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Amy',
+		photo: 'http://icons-for-free.com/free-icons/png/512/1325170.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Bob',
+		photo: 'https://img.playbuzz.com/image/upload/ar_1,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit/v1537904874/ycudsoqgvwbotsuyjtr5.png',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Chis',
+		photo: 'https://c.shld.net/rpx/i/s/pi/mp/21720/prod_15450972109?src=https%3A%2F%2Fblinkee.com%2FGoogleJpegs%2FSmiling-Cool-Dude-Sunny-Days-Sunglasses-Emoji-Flashing-Body-Light-Lapel-Pin.jpg&d=8681df0225e904883dfe759411ef0631b05c7773&?hei=64&wid=64&qlt=50',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Dany',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWXHFv8kb32RSETx4nqJXDFBZXpBEPRMwq8SlhQ1saKhFO4DXF',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Erik',
+		photo: 'https://cdn.shopify.com/s/files/1/1061/1924/products/Frowning_Face_with_Open_Mouth_Emoji_large.png?v=1480481053',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Fran',
+		photo: 'https://img.freepik.com/free-vector/love-emoji_53876-25514.jpg?size=338&ext=jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Greg',
+		photo: 'https://i1.wp.com/leckybang.com/wp-content/uploads/2016/01/tumblr_inline_nwp2q9rRdV1s9clfh_500.png?resize=500%2C350',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Helen',
+		photo: 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/io.C2EwTTKJs/v1/1000x-1.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ian',
+		photo: 'https://data.whicdn.com/images/158587418/superthumb.png',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Justin',
+		photo: 'https://data.whicdn.com/images/247850739/superthumb.png?t=1467582322',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
